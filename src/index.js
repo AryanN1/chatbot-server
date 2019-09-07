ef671d0fbe989ac7191324049956c0183ded900c
Aryan Naimi <aryannaimi@gmail.com>
2019-09-07 13:51:06 -0700

    removed commit history

---
+++ src/index.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const app = express()
+const { NODE_ENV } = process.env
+console.log(NODE_ENV)
+const morganOption = 'common';
+app.use(bodyParser.json())
+app.use(morgan(morganOption))
+app.use(cors())
+app.use(helmet())
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (NODE_ENV === 'production') {
+    response = {
+      error: {
+        message: 'server error'
+      }
+    }
+  } else {
+    console.error(error)
+    response = {
+      message: error.message,
+      error
+    }
+  }
+  res.status(500).json(response)
+})
+
+require('../routes/dialogFlowRoutes')(app);
+console.log('didnt die')
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,8 @@
+const app = require('./index')
+const {
+  PORT
+} = require('./config')
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
