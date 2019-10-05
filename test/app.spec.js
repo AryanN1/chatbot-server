70f3044faa396f09f9c5fbe96c56e1b39c447aee
Aryan Naimi <aryannaimi@gmail.com>
2019-10-05 15:11:14 -0700

    added testing to post routes and fixed readme

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,26 @@
-const app = require('../src/app')
+const app = require("../src/index");
 
-describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+describe("App", () => {
+  let data = {
+    text: "1",
+    userID: "dummy",
+    parameters: []
+  };
+  it("POST /api/df_text_query", () => {
     return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!')
-  })
-})
+      .post("/api/df_text_query")
+      .send(data)
+      .expect(200);
+  });
+  let dataEvent = {
+    event: "1",
+    userID: "dummy",
+    parameters: []
+  };
+  it("POST /api/df_event_query", () => {
+    return supertest(app)
+      .post("/api/df_event_query")
+      .send(dataEvent)
+      .expect(200);
+  });
+});
